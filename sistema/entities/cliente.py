8100e049ee6fda5d1862bd0a491fee73ffb5ceeb
Nicolas Silva <araujo.silva.nicolas@gmail.com>
2023-09-01 21:11:31 -0300

    Adição de descrição para código referente ao reconhecimento do diretório pai

--- sistema/entities/cliente.py
+++ sistema/entities/cliente.py
@@ -4,6 +4,10 @@ import os
 diretorio_pai = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(diretorio_pai)
 
+# O código acima tem a função de reconhecer o diretório pai como caminho absoluto
+# do projeto, evitando erros de reconhecimento de diretório ao importar outros
+# módulos.
+
 from funcoes_gerais import *
 
 class Cliente:
