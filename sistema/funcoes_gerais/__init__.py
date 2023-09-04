055ffbb73acada762dfd5fe46d1e859b70a87201
Nicolas Silva <araujo.silva.nicolas@gmail.com>
2023-09-04 15:59:54 -0300

    Gerador de id removido temporariamente

--- sistema/funcoes_gerais/__init__.py
+++ sistema/funcoes_gerais/__init__.py
@@ -1,4 +1,7 @@
 from funcoes_gerais.gerador import (
-    gerar_id,
     gerar_num_conta
+)
+
+from funcoes_gerais.verificador import (
+    verificar_cpf
 )
--- sistema/funcoes_gerais/gerador.py
+++ sistema/funcoes_gerais/gerador.py
@@ -4,37 +4,6 @@
 
 from random import randint, sample
 
-def gerar_id():
-    novo_id = ''
-    char_check = {}
-
-    while len(novo_id) < 10:
-        num = str(randint(0, 9))
-        letra = str(randint(65, 90))
-
-        if num in char_check:
-            if char_check[num] < 2:
-                novo_id += num
-            
-            char_check[num] += 1
-
-        else:
-            char_check[num] = 1
-            novo_id += num
-
-        if letra in char_check:
-            char_check[letra] += 1
-                    
-            if char_check[letra] < 2:
-                novo_id += letra
-
-        else:
-            char_check[letra] = 1
-            novo_id += letra
-        
-    novo_id = ''.join(sample(novo_id, len(novo_id)))
-    
-    return novo_id
 
 def gerar_num_conta():
     num_conta = str(randint(10000000, 99999999))
@@ -53,3 +22,34 @@ def gerar_num_conta():
 
     return num_conta
 
+# def gerar_id():
+#     novo_id = ''
+#     char_check = {}
+
+#     while len(novo_id) < 10:
+#         num = str(randint(0, 9))
+#         letra = str(randint(65, 90))
+
+#         if num in char_check:
+#             if char_check[num] < 2:
+#                 novo_id += num
+            
+#             char_check[num] += 1
+
+#         else:
+#             char_check[num] = 1
+#             novo_id += num
+
+#         if letra in char_check:
+#             char_check[letra] += 1
+                    
+#             if char_check[letra] < 2:
+#                 novo_id += letra
+
+#         else:
+#             char_check[letra] = 1
+#             novo_id += letra
+        
+#     novo_id = ''.join(sample(novo_id, len(novo_id)))
+    
+#     return novo_id
